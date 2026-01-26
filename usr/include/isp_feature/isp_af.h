435ab21ad4e6216a16f7ab6771bd76e6f7d89cc7
xin03liu <xin03liu@d-robotics.cc>
2026-01-26 15:02:40 +0800

    feat(isp): [X5-3753] Support ROI 3*1 info api [2/2]
    Signed-off-by: xin03liu <xin03liu@d-robotics.cc>

--- usr/include/isp_feature/isp_af.h
+++ usr/include/isp_feature/isp_af.h
@@ -11,6 +11,8 @@
 extern "C" {
 #endif
 
+#include "isp_common.h"
+
 #define HBN_ISP_AFM_BLOCK_POINT_NUM 4		/**< The number of AFM block point numbers */
 #define HBN_ISP_AFM_LDG_Y_NUM 3			/**< The number of AFM Level Depend Gain in Y axis */
 #define HBN_ISP_AFM_LDG_X_NUM 4			/**< The number of AFM Level Depend Gain in X axis */
@@ -19,6 +21,7 @@ extern "C" {
 #define HBN_ISP_AFM_IIR_FILTER_COEFF_NUM 4	/**< The number of AFM IIR filter coefficient */
 #define HBN_ISP_AFM_FIR_HIGHPASS_COEFF_NUM 2	/**< The number of AFM fir highpass filter coefficient*/
 #define HBN_ISP_AFM_FIR_LOWPASS_COEFF_NUM 4	/**< The number of AFM fir lowpass filter coefficient */
+#define HBN_ISP_AFMV1_WINDOW_NUM 3
 
 typedef struct hbn_isp_afm_ldg_s {
 	float iir_highpass_y[HBN_ISP_AFM_LDG_Y_NUM];	/**< IIR high pass on Y axis */
@@ -40,6 +43,7 @@ typedef struct hbn_isp_afm_coring_s {
 
 typedef struct hbn_isp_afm_iir_s {
 	uint8_t data_shift;	/**< Data shift */
+	uint8_t data_shift_v;	/**< Data shift Vetical*/
 	float highpass_weight;	/**< High pass weight */
 	float lowpass_weight;	/**< Low pass weight */
 	float highpass_coeff[HBN_ISP_AFM_IIR_FILTER_NUM][HBN_ISP_AFM_IIR_FILTER_COEFF_NUM];  /**< High pass coefficient */
@@ -48,8 +52,13 @@ typedef struct hbn_isp_afm_iir_s {
 
 typedef struct hbn_isp_afm_fir_s {
 	uint8_t data_shift;		/**< Data shift */
+	uint8_t data_shift_v;		/**< Data shift Vetical*/
 	float highpass_weight;		/**< High pass weight */
 	float lowpass_weight;		/**< Low pass weight */
+	uint8_t highdata_shift;
+	uint8_t highdata_shift_v;
+	uint8_t lowdata_shift;
+	uint8_t lowdata_shift_v;
 	uint8_t highpass_coeff[HBN_ISP_AFM_FIR_HIGHPASS_COEFF_NUM];	/**< High pass coefficient */
 	uint8_t lowpass_coeff[HBN_ISP_AFM_FIR_LOWPASS_COEFF_NUM];	/**< Low pass coefficient */
 } hbn_isp_afm_fir_t;
@@ -64,6 +73,54 @@ typedef struct hbn_isp_afm_attr_s {
 	hbn_isp_afm_iir_t iir;				/**< IIR configuration */
 	hbn_isp_afm_fir_t fir;				/**< FIR configuration */
 } hbn_isp_afm_attr_t;
+
+typedef struct hbn_isp_af_auto_attr_s {
+	uint8_t af_mode;
+
+	// cdaf params
+	float stable_tolerance;		/**< Range: [0, 1] */
+	uint8_t points_of_curve;
+	uint16_t min_focal;		/**< Minimum focal point*/
+	uint16_t max_focal;		/**< Maximum focal point */
+	float motion_threshold;		/**< Motion threshold */
+	uint8_t uphill_allowance;	/**< The range of top hill when CDAF in searching  */
+	uint8_t downhill_allowance;	/**< The range of bottom hill when CDAF in searching */
+
+	// pdaf params
+	float pdconf_threshold;		/**< PDAF configuration threshold */
+	float pdshift_threshold;	/**< Phase Detection shift stable threshold */
+	uint8_t pdstable_count_max;	/**< Maximum focusing to which PDAF locks */
+	float pdaf_unlock_threshold;	/**< PDAF unlock threshold */
+
+	// PDAF and CDAF hybrid params
+	uint8_t defocus_framenum;	/**< The number of defocus frames */
+	uint8_t lossconfidence_framenum;	/**< The number of loss confidence frames */
+	uint8_t accurate_focus_step;	/**< Accurate focus step */
+	uint8_t accurate_focus_enable;	/**< Accurate focus enable */
+} hbn_isp_af_auto_attr_t;
+
+typedef struct hbn_isp_af_attr_s {
+	hbn_isp_mode_e mode;			/**< Support manual mode only */
+	uint32_t position;
+	hbn_isp_af_auto_attr_t auto_attr;
+} hbn_isp_af_attr_t;
+
+typedef struct hbn_isp_afmv1_attr_s {
+	uint32_t threshold;
+	hbn_windows_t afm_windows[HBN_ISP_AFMV1_WINDOW_NUM];
+} hbn_isp_afmv1_attr_t;
+
+typedef struct hbn_isp_afmv1_statistics_s {
+	uint32_t sharpness_a;		/**< Sharpness of window A */
+	uint32_t sharpness_b;		/**< Sharpness of window B */
+	uint32_t sharpness_c;		/**< Sharpness of window C */
+	uint32_t luminance_a;		/**< Luminance of window A */
+	uint32_t luminance_b;		/**< Luminance of window B */
+	uint32_t luminance_c;		/**< Luminance of window C */
+	uint32_t pixelcnt_a;		/**< Pixel counts of window A */
+	uint32_t pixelcnt_b;		/**< Pixel counts of window B */
+	uint32_t pixelcnt_c;		/**< Pixel counts of window C */
+} hbn_isp_afmv1_statistics_t;
 
 #ifdef __cplusplus
 }
